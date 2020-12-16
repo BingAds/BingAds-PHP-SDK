1f1660f630cf8f94522916f2fc606478df365103
Qingjun Tian <qitia@microsoft.com>
2020-12-16 09:06:09 +0800

    update to include production endpoint

--- src/V13/Bulk/BulkServiceSettings.php
+++ src/V13/Bulk/BulkServiceSettings.php
@@ -6,6 +6,7 @@ namespace Microsoft\BingAds\V13\Bulk;
     final class BulkServiceSettings
     {
         const ServiceNamespace = 'https://bingads.microsoft.com/CampaignManagement/v13';
+        const ProductionEndpoint = 'https://bulk.api.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v13/BulkService.svc';
         const SandboxEndpoint = 'https://bulk.api.sandbox.bingads.microsoft.com/Api/Advertiser/CampaignManagement/v13/BulkService.svc';
     }
 
